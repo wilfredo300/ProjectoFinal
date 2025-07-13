bc0d47255651c5c3f7007fe78e7499838e1609f6
datorresbz <datorresb@unjbg.edu.pe>
2025-07-13 08:14:09 -0500

    Mejorando el menu para una mejor visualizacion y implentacion de proximas funciones

--- main.cpp
+++ main.cpp
@@ -10,15 +10,42 @@ int main(){
 extern videojuego tienda[]; //estrae la variable desde tienda.cpp
 char op;
 do{
-    lista_juegos();
-    cout<<  "1) Ordenar por nombre\n"
-            "2) Ordenar por consola\n"
-            "3) Agregar un juego al carrito\n";
-    cout<<"Ingresa una opción: ";
+    system("cls");
+    cout << "----------- TIENDA DE VIDEOJUEGOS -----------\n";
+    cout << "1. Mostrar juegos disponibles\n";
+    cout << "2. Mostrar carrito\n";
+    cout << "3. Mostrar juegos comprados\n";
+    cout << "0. Salir\n";
+    cout << "Ingrese una opcion: ";
+    cin >>op;
+    switch(op){
+        case 1:
+            int subop;
+            system("cls");
+            cout << "--- JUEGOS DISPONIBLES ---\n";
+            cout << "1. Mostrar todos\n";
+            cout << "2. Ordenar alfabeticamente\n";
+            cout << "3. Buscar por consola\n";
+            cout << "4. Seleccionar juego\n";
+            cout << "0. Volver\n";
+            cout << "Opcion: ";
+            cin >> subop;
+            switch(subop){
+                case 1:lista_juegos();
+                case 2:
+                case 3: 
+                case 4:
+                case 0: break;
+
+            }
+        case 2:
+        case 3:
+        case 0:
+    }
     cin>>op;
     op=tolower(op);
     system("pause");
-    system("clear");
+    system("cls");
 } while(op!=0);
 return 0;
 }
