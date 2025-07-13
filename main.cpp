7d59cacc6dc84d5961616a25dfc5ffe5d527a49e
wilfredo300 <junior.val6@gmail.com>
2025-07-12 23:22:12 -0500

    Funcion para mostrar lista de juegos (lista_juegos())

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
+#include<iomanip>
 #include"tienda.h"
 #include"carrito.h"
 using namespace std;
+
 int main(){
     
-extern videojuego tienda[];
-for(int i=0;i<5;i++){
-    cout<<i+1<<") ";tienda[i].imprimir();
-}
- 
+//muestra la lista de video juegos
+extern videojuego tienda[]; //estrae la variable desde tienda.cpp
+char op;
+do{
+    lista_juegos();
+    cout<<  "1) Ordenar por nombre\n"
+            "2) Ordenar por consola\n"
+            "3) Agregar un juego al carrito\n";
+    cout<<"Ingresa una opción: ";
+    cin>>op;
+    op=tolower(op);
+    system("pause");
+    system("clear");
+} while(op!=0);
 return 0;
 }
+
+
